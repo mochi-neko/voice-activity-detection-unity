a4367c1ea8dcfdc158b9ebefb88621fc7ee67f65
mochi-neko <t.o.e.4315@gmail.com>
2023-07-30 09:28:18 +0900

    Add sample for AudioSource

---
+++ Assets/Mochineko/VoiceActivityDetection.Samples/VADAudioSourceSample.cs
@@ -0,0 +1,80 @@
+#nullable enable
+using System;
+using UniRx;
+using Unity.Logging;
+using UnityEngine;
+
+namespace Mochineko.VoiceActivityDetection.Samples
+{
+    /// <summary>
+    /// A sample of voice activity detection as a component.
+    /// Input UnityEngine.AudioSource and output only log.
+    /// </summary>
+    internal sealed class VADAudioSourceSample : MonoBehaviour
+    {
+        [SerializeField]
+        private VADParameters? parameters = null;
+
+        [SerializeField]
+        private AudioSource? audioSource = null;
+
+        private IVoiceActivityDetector? vad;
+        private UnityMicrophoneProxy? proxy;
+        private UnityAudioSource? source;
+
+        private void Start()
+        {
+            if (parameters == null)
+            {
+                throw new NullReferenceException(nameof(parameters));
+            }
+
+            if (audioSource == null)
+            {
+                throw new NullReferenceException(nameof(audioSource));
+            }
+
+            source = new UnityAudioSource();
+
+            vad = new QueueingVoiceActivityDetector(
+                source: source,
+                buffer: new NullVoiceBuffer(),
+                parameters.MaxQueueingTimeSeconds,
+                parameters.MinQueueingTimeSeconds,
+                parameters.ActiveVolumeThreshold,
+                parameters.ActivationRateThreshold,
+                parameters.InactivationRateThreshold,
+                parameters.ActivationIntervalSeconds,
+                parameters.InactivationIntervalSeconds,
+                parameters.MaxActiveDurationSeconds);
+
+            vad
+                .VoiceIsActive
+                .Subscribe(isActive => Log.Debug("[VAD.Sample] IsActive: {0}", isActive))
+                .AddTo(this);
+
+            // Set microphone as audio source
+            proxy = new UnityMicrophoneProxy();
+            var audioClip = proxy.AudioClip;
+            audioSource.clip = audioClip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+        private void OnDestroy()
+        {
+            proxy?.Dispose();
+            vad?.Dispose();
+        }
+
+        private void Update()
+        {
+            vad?.Update();
+        }
+
+        private void OnAudioFilterRead(float[] data, int channels)
+        {
+            source?.OnAudioFilterRead(data, channels);
+        }
+    }
+}
